8868ebadc2ebf30c0ceeab869f8039ea794f2680
K KARTHIK BHAT <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-01-22 00:34:15 +0530

    Update thumbnails.py

--- FallenX/utils/thumbnails.py
+++ FallenX/utils/thumbnails.py
@@ -1,7 +1,7 @@
 import os
 import re
 import textwrap
-
+import random
 import aiofiles
 import aiohttp
 import numpy as np
@@ -222,7 +222,7 @@ async def gen_qthumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"FallenX/assets/test.png")
+        bg = Image.open(f"FallenX/assets/test.png","FallenX/assets/test1.png","FallenX/assets/test2.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
